d4652f553042ee33ef39c4ae2f42c192c8bf66f2
ssemugenyi <ssemugenyiisaac2@gmail.com>
2024-08-20 14:09:24 +0300

    feat: added firbaase config

---
+++ src/store/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC29Rgju2nPWHa3uD2acw5vHTGvHOZgoKc",
+  authDomain: "sacco-platform.firebaseapp.com",
+  projectId: "sacco-platform",
+  storageBucket: "sacco-platform.appspot.com",
+  messagingSenderId: "714744749015",
+  appId: "1:714744749015:web:8f7758babb4aa3f3b34992",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
