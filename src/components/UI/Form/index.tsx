0b032d5c56b5b38e4b0d3d2731fd11144a94f497
ssemugenyi <ssemugenyiisaac2@gmail.com>
2024-08-21 13:49:27 +0300

    aaded logo

--- src/components/UI/Form/index.tsx
+++ src/components/UI/Form/index.tsx
@@ -1,5 +1,5 @@
 import React, { type ReactNode } from "react";
-
+import Logo from "../../../assets/images/221-logo.jpg";
 interface FormProps {
   title?: string;
   description?: string;
@@ -15,6 +15,9 @@ const Form = ({ title, description, children, onSubmit }: FormProps) => {
       noValidate
     >
       <div className="flex items-center justify-center sm:hidden"></div>
+      <div className="flex items-center justify-center w-full">
+        <img src={Logo} alt="212 logo" className="w-32 h-32" />
+      </div>
       <div className="text-center mb-4">
         <h1 className="font-poppins font-bold text-2xl text-textColor capitalize mb-2">
           {title}
